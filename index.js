1ba6d8889dc7a03023ad677765b25b3662c9175f
Duncan <duncan.moiyo@gmail.com>
2022-07-23 15:51:30 +0300

    create a function for making tweets

---
+++ index.js
@@ -0,0 +1,16 @@
+import { TwitterApi } from "twitter-api-v2";
+import dotenv from "dotenv";
+dotenv.config();
+
+const client = new TwitterApi({
+  appKey: process.env.API_KEY,
+  appSecret: process.env.API_KEY_SECRET,
+  accessToken: process.env.ACCESS_TOKEN,
+  accessSecret: process.env.ACCESS_TOKEN_SECRET,
+});
+async function makeTweet() {
+  const tweet = await client.v1.tweet(
+    "Hello ,world! I'm twitting from my Node.js app!"
+  );
+}
+makeTweet();
